350af8632a1b4497af4b5424b6d96b9817fab154
Shivam68 <amansingh682006@gmail.com>
2026-06-13 14:54:30 +0530

    Added this file

---
+++ Day-13/problem49.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter size: ";
+    cin>>n;
+
+    int arr[n];
+
+    cout<<"Enter elements: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    cout<<"Array elements are: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
