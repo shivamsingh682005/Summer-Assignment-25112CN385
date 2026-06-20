02797b7fb1db431b96db6018ce40aa43610137cd
Shivam68 <amansingh682006@gmail.com>
2026-06-20 20:16:30 +0530

    Add this file

---
+++ Day-20/problem79.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, c;
+    cout << "Enter rows and cols: ";
+    cin >> r >> c;
+
+    int a[10][10];
+
+    cout << "Enter matrix:\n";
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            cin >> a[i][j];
+
+    for(int i = 0; i < r; i++) {
+        int sum = 0;
+
+        for(int j = 0; j < c; j++) {
+            sum += a[i][j];
+        }
+
+        cout << "Sum of Row " << i + 1 << " = " << sum << endl;
+    }
+
+    return 0;
+}
